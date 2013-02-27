9f5797805f9d211f61ab6168e75fad6993e7cd0a
Speransky Danil <speranskydanil@gmail.com>
2013-02-27 15:00:03 +0700

    init

---
+++ README.md
@@ -0,0 +1,70 @@
+# Simple-Graffiti
+
+Simple-Graffiti is a JavaScript library which uses canvas.
+It consists of two parts: core and example of it's usage.
+
+## Graffiti-Core
+
+Graffiti-Core is a base for graffiti application. It provides drawning and JavaScript API.
+
+```
+<script src="js/jquery.js"></script>
+<script src="js/graffiti-core.js"></script>
+
+<div id="graffiti"></div>
+
+// initialization
+gc = new GraffitiCore({
+  selector: '#graffiti',
+
+  width: 600,
+  height: 300,
+
+  brush: {
+    width: 40,
+    color: 'rgba(200, 200, 50, 0.75)'
+  }
+});
+
+// api
+
+gc.prev();
+gc.next();
+gc.clear();
+alert(gc.data());
+gc.setBrush({ 
+  width: 40, 
+  color: 'rgba(200, 200, 50, 0.75)'
+});
+```
+
+![screen](https://raw.github.com/speranskydanil/Simple-Graffiti/master/screen-core.png)
+
+## Graffiti-Application
+
+Graffiti-Application is an example of using the core.
+
+### Get Start
+
+```
+<link rel="stylesheet" href="css/graffiti-application.css">
+
+<script src="js/jquery.js"></script>
+<script src="js/graffiti-core.js"></script>
+<script src="js/graffiti-application.js"></script>
+
+new GraffitiApplication({
+  selector: '.ga',
+
+  width: 600,
+  height: 300,
+
+  brush: {
+    width: 40,
+    color: 'rgba(200, 200, 50, 0.75)'
+  }
+});
+```
+
+![screen](https://raw.github.com/speranskydanil/Simple-Graffiti/master/screen-application.png)
+
---
+++ js/graffiti-application.js
@@ -0,0 +1,118 @@
+var GraffitiApplication = (function ($) {
+  function Class(conf) {
+    var init = conf.init || function () {};
+    delete conf.init;
+    init.prototype = conf;
+    return init;
+  };
+  
+  var GraffitiApplication = new Class({
+    init: function (conf) {
+      conf = this.initConf(conf);
+
+      this.buildDom(conf);
+      this.bindEvents(conf);
+    },
+
+    initConf: function (conf) {
+      var _conf = {
+        selector: undefined,
+
+        width: 600,
+        height: 300,
+          
+        brush: {
+          width: 40,
+          color: 'rgba(200, 200, 50, 0.75)'
+        }
+      };
+
+      for (var prop in conf) {
+        _conf[prop] = conf[prop];
+      }
+
+      return _conf;
+    },
+
+    buildDom: function (conf) {
+      $(conf.selector).html('\
+        <div class="ga-viewport"></div>\
+        <div class="ga-nav">\
+          <a href="#" class="prev">prev</a>\
+          |\
+          <a href="#" class="next">next</a>\
+          <br>\
+          <a href="#" class="clear">clear</a>\
+          |\
+          <a href="#" class="save">save</a>\
+          <br>\
+        </div>\
+        <div class="ga-brush">\
+          <input type="text" class="width"> &nbsp;width<br>\
+          <input type="text" class="opacity"> &nbsp;opacity<br>\
+          <input type="text" class="red"> &nbsp;red<br>\
+          <input type="text" class="green"> &nbsp;green<br>\
+          <input type="text" class="blue"> &nbsp;blue<br>\
+        </div>\
+      ');
+
+      this.dom = {
+        nav: $(conf.selector + ' .ga-nav'),
+        brush: $(conf.selector + ' .ga-brush')
+      };
+
+      var color_pattern = /^\s*rgba\(\s*(\d+)\s*\,\s*(\d+)\s*\,\s*(\d+)\s*\,\s*(\d*\.?\d+)\s*\)\s*$/;
+      var color_matches = conf.brush.color.match(color_pattern).slice(1);
+
+      this.dom.brush.find('.width').val(conf.brush.width);
+      this.dom.brush.find('.opacity').val(color_matches[3]);
+      this.dom.brush.find('.red').val(color_matches[0]);
+      this.dom.brush.find('.green').val(color_matches[1]);
+      this.dom.brush.find('.blue').val(color_matches[2]);
+
+      this.gc = new GraffitiCore({
+        selector: conf.selector + ' > .ga-viewport',
+        width: conf.width,
+        height: conf.height,
+        brush: conf.brush
+      });
+    },
+
+    bindEvents: function (conf) {
+      var self = this;
+
+      this.dom.nav.find('a').click(function (e) {
+        e.preventDefault();
+
+        switch($(this).attr('class')) {
+          case 'prev':
+            self.gc.prev();
+            break;
+          case 'next':
+            self.gc.next();
+            break;
+          case 'clear':
+            self.gc.clear();
+            break;
+          case 'save':
+            window.open(self.gc.data());
+            break;
+        }
+      });
+
+      this.dom.brush.find('input').change(function (e) {
+        self.gc.setBrush({
+          width: parseInt(self.dom.brush.find('.width').val()),
+          color: 'rgba(' + 
+                 parseInt(self.dom.brush.find('.red').val()) + ',' + 
+                 parseInt(self.dom.brush.find('.green').val()) + ',' + 
+                 parseInt(self.dom.brush.find('.blue').val()) + ',' + 
+                 parseFloat(self.dom.brush.find('.opacity').val()) + ')'
+        });
+      });
+    }
+  });
+
+  return GraffitiApplication;
+})(jQuery);
+
